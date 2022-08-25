0217268612e3156ca56bc116a7eb1fe3b23af5fa
Deadpool1 <atwood169@yahoo.com>
2022-08-24 21:05:11 -0500

    created thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            // must be between 1 and 380 characters
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reaction',
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+
+
+module.exports = Thought
